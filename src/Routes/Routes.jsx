68e9abb5bf2c01155daa9ae2109dd2fa20cc3084
fahimmutashir23 <taslimit23@gmail.com>
2023-10-18 14:20:32 +0600

    basic setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Pages/Home/Home";
+import Cart from "../Pages/AddCart/Cart";
+import AddProducts from "../Pages/AddProducts/AddProducts";
+import Login from "../Pages/Login/Login";
+
+const Routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout></Layout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/cart",
+                element: <Cart></Cart>
+            },
+            {
+                path: "/addProducts",
+                element: <AddProducts></AddProducts>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            }
+        ]
+    }
+])
+
+export default Routes;
